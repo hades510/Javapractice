a723061f43a2ff5de8d60b0708755c47f3fc3d34
hades510 <saassi143@gmail.com>
2023-06-20 21:41:02 +0545

    First push

---
+++ LocalVscope.java
@@ -0,0 +1,15 @@
+public class LocalVscope {
+    void method() {
+        int x=5;
+        System.out.println(x);
+    }
+}
+/**Variable scopes
+ * local variable scope
+    defined inside a block,method body 
+    or constructor
+ *cannot be accessed outside the method.
+ *Member/class level variable scope
+    variable declared inside a class 
+    but, outside the method.
+ */
